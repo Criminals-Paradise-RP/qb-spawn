4f8bfdf116a00d9110b9890386fa671eaa02f011
JordanBScott <99845881+JordanBScott@users.noreply.github.com>
2023-12-16 17:58:24 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,58 +1,58 @@
 QB = {}
 
 QB.Spawns = {
-    ["legion"] = {
-        coords = vector4(121.67, -879.72, 31.12, 248.88),
-        location = "legion",
-        label = "Legion Square",
-    },
+    -- ["legion"] = {
+    --     coords = vector4(121.67, -879.72, 31.12, 248.88),
+    --     location = "legion",
+    --     label = "Legion Square",
+    -- },
 
-    ["policedp"] = {
-        coords = vector4(457.0, -969.65, 30.71, 355.62),
-        location = "policedp",
-        label = "LS Police",
-    },
+    -- ["policedp"] = {
+    --     coords = vector4(457.0, -969.65, 30.71, 355.62),
+    --     location = "policedp",
+    --     label = "LS Police",
+    -- },
 
-    ["statepolice"] = {
-        coords = vector4(1533.48, 820.68, 77.5, 51.49),
-        location = "statepolice",
-        label = "State Police",
-    },
+    -- ["statepolice"] = {
+    --     coords = vector4(1533.48, 820.68, 77.5, 51.49),
+    --     location = "statepolice",
+    --     label = "State Police",
+    -- },
 
-    ["lsjc"] = {
-        coords = vector4(-239.42, -909.88, 32.31, 188.59),
-        location = "lsjc",
-        label = "Job Center",
-    },
+    -- ["lsjc"] = {
+    --     coords = vector4(-239.42, -909.88, 32.31, 188.59),
+    --     location = "lsjc",
+    --     label = "Job Center",
+    -- },
 
-    ["ssmedical"] = {
-        coords = vector4(1682.29, 3646.37, 35.5, 267.76),
-        location = "ssmedical",
-        label = "Sandy Hospital",
-    },
+    -- ["ssmedical"] = {
+    --     coords = vector4(1682.29, 3646.37, 35.5, 267.76),
+    --     location = "ssmedical",
+    --     label = "Sandy Hospital",
+    -- },
 
-    ["hunting"] = {
-        coords = vector4(-760.32, 5588.68, 36.71, 229.59),
-        location = "hunting",
-        label = "Hunting Shop",
-    },
+    -- ["hunting"] = {
+    --     coords = vector4(-760.32, 5588.68, 36.71, 229.59),
+    --     location = "hunting",
+    --     label = "Hunting Shop",
+    -- },
 
-    ["airport"] = {
-        coords = vector4(-952.02, -2753.21, 13.76, 2.32),
-        location = "airport",
-        label = "LS Airport",
-    },
+    -- ["airport"] = {
+    --     coords = vector4(-1035.71, -2731.87, 12.86),
+    --     location = "airport",
+    --     label = "LS Airport",
+    -- },
 
-    ["pier"] = {
-        coords = vector4(-1856.52, -1230.81, 13.02, 299.57),
-        location = "pier",
-        label = "Del Perro Pier",
-    },
+    -- ["pier"] = {
+    --     coords = vector4(-1856.52, -1230.81, 13.02, 299.57),
+    --     location = "pier",
+    --     label = "Del Perro Pier",
+    -- },
 
-    ["courthouse"] = {
-        coords = vector4(-557.36, -193.03, 38.23, 233.88),
-        location = "courthouse",
-        label = "City Hall",
-    },
+    -- ["courthouse"] = {
+    --     coords = vector4(-557.36, -193.03, 38.23, 233.88),
+    --     location = "courthouse",
+    --     label = "City Hall",
+    -- },
 
 }
